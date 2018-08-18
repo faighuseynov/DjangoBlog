bdcfea7a90ef6a9280bd556143e925c8ea4bfd37
faig <faiq.huseynov96@gmail.com>
2018-08-18 14:57:18 +0400

    few additions

--- article/models.py
+++ article/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey('auth.User', on_delete='models.CASCADE', verbose_name='Yazıçı')
+    title = models.CharField(max_length=50, verbose_name='Başlıq')
+    content = RichTextField()
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Qeyd edilmə tarixi')
+    article_image = models.FileField(blank=True, null=True, verbose_name='Məqaləyə şəkil əlavə edin')
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_date']
+
+
+
